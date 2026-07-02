a460fd1a908bc852ca6ac27fd9928a58896feb56
sithija-anuradha <sithijaanuradhalap2004@gmail.com>
2026-07-02 09:18:04 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,58 @@
+// Password
+const correctPassword = "Nuheli";
 
+// Elements
+const passwordInput = document.getElementById("password");
+const loginBtn = document.getElementById("loginBtn");
+const togglePassword = document.getElementById("togglePassword");
+
+// Show / Hide Password
+togglePassword.addEventListener("click", () => {
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        togglePassword.textContent = "🙈";
+    } else {
+        passwordInput.type = "password";
+        togglePassword.textContent = "👁";
+    }
+
+});
+
+// Login
+function login() {
+
+    const password = passwordInput.value.trim();
+
+    if (password === correctPassword) {
+
+        loginBtn.textContent = "Loading...";
+
+        setTimeout(() => {
+
+            window.location.href = "movies.html";
+
+        }, 1000);
+
+    } else {
+
+        alert("Password Eka Waradi Pancho😪");
+
+        passwordInput.value = "";
+        passwordInput.focus();
+
+    }
+
+}
+
+// Button click
+loginBtn.addEventListener("click", login);
+
+// Press Enter
+passwordInput.addEventListener("keydown", function (e) {
+
+    if (e.key === "Enter") {
+        login();
+    }
+
+});
